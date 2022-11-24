3824d6a04ae6e92153e2ad9e1047ba430724d540
Jay Wilson <jaywilson@Jays-iMac.local>
2022-11-24 22:48:07 +1000

    function tests

---
+++ tests/functions/debug.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace JWWS\WP_Plugin_Framework\Tests\Functions\Debug;
+
+use function JWWS\WP_Plugin_Framework\Functions\Debug\console_log;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+console_log('Test');
