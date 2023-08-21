89355865ea5fc7e474f70dd149b56625a3e9368e
Jay Wilson <jayanthonyjohnwilson@gmail.com>
2023-08-21 19:43:08 +1000

    Update jwws-wp-plugin-framework.php

--- jwws-wp-plugin-framework.php
+++ jwws-wp-plugin-framework.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:  WP Plugin Framework
  * Description:  Framework for JWWS plugins.
- * Version:      3.5
+ * Version:      3.5b1
  * Requires PHP: 8.1
  * Author:       Jay Wilson
  * Author URI:   https://jaywilsonwebsolutions.com
